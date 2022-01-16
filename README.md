71f17853eefcee24a2bd280a0a843ff0cf8cb663
rsh456 <rsh456@hotmail.com>
2022-01-16 12:39:38 -0500

    Database package to wire into rocket service

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ as a comment where Store indicates the interface to be mocked
 > go test ./... -v
 
 ![Test result](https://github.com/rsh456/go-grpc-services/blob/master/images/test_1.PNG)
+
+3. Running postgres locally with Docker
+>docker run --name rocket-inventory-db -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/rsh456/go-grpc-services/internal/db"
+	"github.com/rsh456/go-grpc-services/internal/rocket"
+)
 
 func Run() error {
 	//Responsible for initializing and starting
 	//gRPC server
+	rocketStore, err := db.New()
+	if err != nil {
+		return err
+	}
+	_ = rocket.New(rocketStore)
+
 	return nil
 }
 
